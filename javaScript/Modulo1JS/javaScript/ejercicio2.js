cb92c6ddc91ce70e1562d60393ba6dff2ff2dcb5
oscar vallejo <onvallejo35@gmail.com>
2023-08-10 16:05:57 -0300

    Ejercicio 2 - Desafio 1 - Modulo 7 JavaScript

--- javaScript/Modulo1JS/javaScript/ejercicio2.js
+++ javaScript/Modulo1JS/javaScript/ejercicio2.js
@@ -1,8 +1,15 @@
 let presupuesto = Number(prompt('ingrese su presupuesto total: '))
 
-document.write('Si su presupuesto personal es de $' + presupuesto + ', entonces sus finanzas personales quedarian de la siguiente manera:')
-document.write('<br/>Gasto necesario = $' + presupuesto * 0.5) 
-document.write('<br/>Gasto opcional = $' + presupuesto * 0.3) 
-document.write('<br/>Ahorro = $' + presupuesto * 0.2);
+if(presupuesto){
+    document.write('Si su presupuesto personal es de $. ' + presupuesto + ', entonces sus finanzas personales quedarian de la siguiente manera:')
+document.write('<br/>Gasto necesario => $' + presupuesto * 0.5) 
+document.write('<br/>Gasto opcional => $' + presupuesto * 0.3) 
+document.write('<br/>Ahorro => $' + presupuesto * 0.2);
+
+}else{
+ alert('Incorrecto... Ingrese dato númerico!d')   
+};
+
+
 
 
