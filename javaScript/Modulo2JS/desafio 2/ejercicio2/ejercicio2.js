255d91358318e97821468b5e74e9da4decf1f709
oscar vallejo <onvallejo35@gmail.com>
2023-08-10 15:31:36 -0300

    Ejercicio2 - Desafio2 - Modulo7 JS

--- javaScript/Modulo2JS/desafio 2/ejercicio2/ejercicio2.js
+++ javaScript/Modulo2JS/desafio 2/ejercicio2/ejercicio2.js
@@ -4,12 +4,4 @@ const clave = prompt('Ahora su clave: ');
 const usuarioValido = usuario === 'admin';
 const claveValida = Number(clave) === 1234;
 
-/*
-if (usuarioValido && claveValida) {
-    alert('¡Sea Usted Bienvenido!');
-}else {
-    alert('Sus credenciales son incorrectas !!!');
-}
-*/
-
 usuarioValido && claveValida ? alert('Bienvenido!') : alert('Sus credenciales son incorrectas!!!')
