01fc4dc3cf822851a4447a93df0818d086d7187d
oscar vallejo <onvallejo35@gmail.com>
2023-08-10 15:49:20 -0300

    Ejercicio1 - Desafio1 - Modulo 7 JS

--- javaScript/Modulo1JS/javaScript/ejercicio1.js
+++ javaScript/Modulo1JS/javaScript/ejercicio1.js
@@ -5,8 +5,8 @@ let costosVariables = Number(prompt('ingrese valor costos variables:'));
 let puntoEquilibrio = costosFijos / (precioVenta - costosVariables)
 
 // los costos variables no pueden ser mayor que el precio de venta.
-if (costosVariables >= precioVenta) {
+if (costosVariables >= precioVenta != 0) {
         alert("No es posible calcular el punto de equilibrio. Los costos variables son mayores o iguales al precio de venta.")
     }   else {
         alert('El punto de equilibrio es igual a: ' + puntoEquilibrio)
-    }
+    };
